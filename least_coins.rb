9b308eff77ad2c115e9726ed9486557b5d316951
Uma Desai <umadesai38@gmail.com>
2018-07-11 23:42:40 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,19 @@
 #write out your code here
 
 def least_coins(cents)
-
-#Code your answer here!
-
+  coin_types= {
+    :quarters => 0
+    :dimes => 0
+    :nickels => 0
+    :pennies => 0
+  }
+  
 end
+
+quarters=cents/25
+if cents => 25
+  puts "quarters=> 0"
+elsif cents<= 25
+puts "quarters= 1"
+end
+
